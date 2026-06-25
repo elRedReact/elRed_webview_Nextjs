9aa986bb67d07896fa7023504e5071bfaed4840d
TashafMahmood <tashaf@elred.io>
2026-06-25 12:50:03 +0530

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { LogoutProvider } from "@/context/LogoutContext";
 import Head from "next/head";
 
 
-console.log("Env: Pretest | Account: ProductionPrototype - new");
+console.log("Env: Dev");
 export default function App({ Component, pageProps }) {
 
   return (
