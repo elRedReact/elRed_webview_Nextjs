895c9ae7e6e6d54515762f1161e5c9a43e1af8a9
TashafMahmood <tashaf@elred.io>
2026-06-25 10:03:47 +0530

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { LogoutProvider } from "@/context/LogoutContext";
 import Head from "next/head";
 
-console.log(" deployed on test...");
+console.log("deployed on test...");
 
 export default function App({ Component, pageProps }) {
   // Check if page defines its own layout
